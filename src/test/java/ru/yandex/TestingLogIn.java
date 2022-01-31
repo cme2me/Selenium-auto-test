4d6db57219681b46959ed1afe44ceccb954a7e60
cme2me <max.zhakov@gmail.com>
2022-01-31 16:16:14 +0300

    Implemented a method that counts how many times an email has been sent

--- src/test/java/ru/yandex/TestingLogIn.java
+++ src/test/java/ru/yandex/TestingLogIn.java
@@ -9,8 +9,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class TestingLogIn {
     public static WebDriver driver;
@@ -39,4 +43,30 @@ public class TestingLogIn {
         driver.findElement(By.id("passp-field-passwd")).sendKeys("testing123123");
         driver.findElement(By.id("passp:sign-in")).click();
     }
+    public class Page{
+        @FindBy(xpath = "//*[contains(@class, 'mail-ComposeButton-Text')]")
+                private WebElement writeButton;
+        @FindBy(xpath = "//*[contains(@class, 'composeYabbles')]")
+                private WebElement addressBoard;
+        @FindBy(xpath = "//*[contains(@class, 'composeTextField ComposeSubject-TextField')]")
+                private WebElement subjBoard;
+        @FindBy(xpath = "//*[contains(@class, 'cke_wysiwyg_div cke_reset')]")
+                private WebElement contentBoard;
+        @FindBy(xpath = "//*[contains(@class, 'ComposeControlPanelButton-Button_action')]")
+                private WebElement sendBnt;
+        public WebDriver driver;
+
+        public Page(WebDriver driver){
+            PageFactory.initElements(driver, this);
+            this.driver = driver;
+        }
+
+        public int getMailsNum(){
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), '<<Simbirsoft Тестовое задание>>')]")));
+
+            List<WebElement> Emails = driver.findElements(By.xpath("//*[contains(text(), '<<Simbirsoft Тестовое задание>>')]"));
+            return Emails.size();
+        }
+    }
+
 }
